5161cde02f93db91b88e0643ce6f99c28d2ba1e7
Musab Ahmed Khan Umair <110651555+musabaku@users.noreply.github.com>
2024-05-20 11:25:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+  from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+                age=float(request.form.get('age')),
+                tsh=float(request.form.get('tsh')),
+                t3=float(request.form.get('t3')),
+                t4=float(request.form.get('t4'))
+        )
+
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
